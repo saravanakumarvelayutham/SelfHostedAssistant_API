69247bb54ea1110594ee87a8d25f52b93790c4e3
saravanakumar velayutham <saravanakumar.velayutham@mavs.uta.edu>
2019-11-17 19:30:35 -0600

    removed database name

--- SelfHostedAssistant/Models/AssistantDatabaseSettings.cs
+++ SelfHostedAssistant/Models/AssistantDatabaseSettings.cs
@@ -4,14 +4,10 @@ namespace SelfHostedAssistant.Models
     public class AssistantDatabaseSettings : IAssistantDatabaseSettings
     {
         public string ConnectionString { get; set; }
-
-        public string DatabaseName { get; set; }
     }
 
     public interface IAssistantDatabaseSettings
     {
         string ConnectionString { get; set; }
-
-        string DatabaseName { get; set; }
     }
 }
